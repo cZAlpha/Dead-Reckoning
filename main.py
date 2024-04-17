186f1271201a568cf919438cd77ce0daaf66d895
cZAlpha <nbklaus21@students.desu.edu>
2024-04-17 13:18:00 -0400

    Vectorized The Program

--- main.py
+++ main.py
@@ -1,35 +1,32 @@
+import numpy as np
+
 class Object:
-    def __init__(self, x, y, velocity_x, velocity_y):
-        self.x = x
-        self.y = y
-        self.velocity_x = velocity_x
-        self.velocity_y = velocity_y
+    def __init__(self, position, velocity):
+        self.position = np.array(position)
+        self.velocity = np.array(velocity)
 
     def update_position(self, time):
-        self.x += self.velocity_x * time
-        self.y += self.velocity_y * time
+        self.position += self.velocity * time
 
 def main():
     # Initial object parameters
-    initial_x = 0  # Initial x-coordinate
-    initial_y = 0  # Initial y-coordinate
-    velocity_x = 5  # Initial velocity in x-direction
-    velocity_y = 3  # Initial velocity in y-direction
+    initial_position = [0, 0]  # Initial position [x, y]
+    velocity = [5, 3]  # Initial velocity [vx, vy]
 
     # Time interval
     time = 2  # Time interval for estimation
 
     # Create object
-    obj = Object(initial_x, initial_y, velocity_x, velocity_y)
+    obj = Object(initial_position, velocity)
 
     # Print initial position
-    print("Initial position: ({}, {})".format(obj.x, obj.y))
+    print("Initial position:", obj.position)
 
     # Update position using dead reckoning
     obj.update_position(time)
 
     # Print estimated position
-    print("Estimated position after {} seconds: ({}, {})".format(time, obj.x, obj.y))
+    print("Estimated position after {} seconds:".format(time), obj.position)
 
 if __name__ == "__main__":
     main()
