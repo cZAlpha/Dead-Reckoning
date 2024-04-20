8c5f8e83fac9d2fe541a4d2d24e3bf4286b53e64
cZAlpha <nbklaus21@students.desu.edu>
2024-04-20 13:48:01 -0400

    Merged Rabbit Sim with this project

--- main.py
+++ main.py
@@ -18,6 +18,34 @@ class Object:
             print("Acceleration:", self.acceleration)
             print("Velocity:", self.velocity)
 
+def rabbit():
+    # Define parameters
+    r = 0.1  # growth rate
+    K = 1000  # carrying capacity
+    dt = 0.1  # time step size
+    num_steps = 1000  # number of time steps
+
+    # Initialize array to store population sizes
+    N_values = np.zeros(num_steps + 1)
+    N_values[0] = 10  # initial population size
+
+    # Iterate over time steps to compute population sizes
+    for n in range(num_steps):
+        N_values[n + 1] = N_values[n] + r * dt * N_values[n] * (1 - N_values[n] / K)
+
+    # Plot population size over time
+    plt.plot(np.arange(num_steps + 1) * dt, N_values, label='Population Size')
+    plt.xlabel('Time')
+    plt.ylabel('Population size')
+    plt.title('Rabbit Population Growth (Difference Equation)')
+    plt.grid(True)
+
+    # Highlight the sequence of population sizes
+    for n in range(num_steps):
+        plt.plot([n * dt, (n + 1) * dt], [N_values[n], N_values[n + 1]], 'w--')  # Make the line dashed
+
+    plt.legend()
+    plt.show()
 
 
 def linear():
@@ -143,6 +171,7 @@ def varying_velocity():
         print("Estimated position after {} seconds: {}".format(time_intervals[i - 1], pos))
 
 if __name__ == "__main__":
+    rabbit()
     linear()
     second_order()
     varying_velocity()
